ce7cee30cda2652f7dc5db3995ae02969e616088
aniktonu <aniktonu@gmail.com>
2022-12-29 17:44:00 +0600

    apps file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Usestate from './components/Usestate';
+import UseEffect from './components/Useeffect';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+import Usecallback from './components/Usecallback';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 
 function App() {
   return (
-    <>
-      <h1>React Boilerplate</h1>
-    </>
+    <div className='App'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/usestate" element={<Usestate />} />
+          <Route path="/useeffect" element={<UseEffect />} />
+          <Route path="/usecallback" element={<Usecallback />} />
+
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
