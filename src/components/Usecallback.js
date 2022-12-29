bc4a9e654af83a2c10f71e82bc2a2967ec5a892e
aniktonu <aniktonu@gmail.com>
2022-12-29 17:57:40 +0600

    reset button added

--- src/components/Usecallback.js
+++ src/components/Usecallback.js
@@ -5,6 +5,7 @@ export default function Usecallback() {
 
     const [count, setCount] = useState(0)
     const [count2, setCount2] = useState(0)
+    const [isShown, setIsShown] = useState(false);
 
 
     const Increment = () => {
@@ -19,26 +20,33 @@ export default function Usecallback() {
         setCount2(0)
     }
 
-    const buttonShow = () => {
+    const showRestButton = (e) => {
 
+        setIsShown(true);
 
-
-        return (
-            <div className='Height'>
-                <h4 className="text-primary"><u>React useCallBack Hook</u></h4>
-                <h3>{count}</h3>
-                <button type="button" onClick={Increment}>Click to increment</button>
-                <h3>{count2}</h3>
-                <button type="button" onClick={Increment2}>Click to increment</button>
-                <br />
-                <button type="button" onClick={Reset}>Click to Reset</button>
-
-
-
-
-
-
-            </div>
-
-        )
     }
+
+
+
+    return (
+        <div className='Height'>
+            <h4 className="text-primary"><u>React useCallBack Hook</u></h4>
+            <h3>{count}</h3>
+            <button type="button" onClick={() => { Increment(); showRestButton(); }}>Click to increment</button>
+            <h3>{count2}</h3>
+            <button type="button" onClick={Increment2}>Click to increment</button>
+            <br />
+            {isShown && (
+                <button type="button" onClick={Reset}>Click to Reset</button>
+            )}
+
+
+
+
+
+
+
+        </div>
+
+    )
+}
